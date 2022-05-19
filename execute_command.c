c136a879a0ba93201703b1cc8018f2f1b0eca3b0
Daniel Njeru <danofficial014@gmail.com>
2022-05-19 13:03:36 +0300

    all files

---
+++ execute_command.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ * execute_command - executes command entered by user
+ * @arr: tokenized string entered by user
+ */
+void execute_command(char **arr)
+{
+	pid_t my_pid;
+	int status;
+
+	my_pid = fork();
+	if (my_pid == -1)
+	{
+		perror("Unable to create child process");
+		return;
+	}
+	if (my_pid == 0)
+	{
+		if (execve(arr[0], arr, NULL) == -1)
+		{
+			perror("Unable to execute");
+			_exit(0);
+		}
+		_exit(0);
+	}
+	else
+		wait(&status);
+}
